24dcfd8b447ef5e8e9b9daa170aef68533b20c17
Henry <vinsen1109@gmail.com>
2021-02-22 17:06:03 +0800

    add commas_class & test

---
+++ commas_test.rb
@@ -0,0 +1,25 @@
+require './commas'
+
+RSpec.describe Commas do
+  describe 'test' do
+
+    before do
+      @commas = Commas.new
+    end
+
+    it '每三位數一個逗點' do
+      expect(@commas.set(9527)).to eq '9,527'
+      expect(@commas.set(3345678)).to eq '3,345,678'
+    end
+
+    # it '顯示小數位' do
+    #   expect(1234.45).to be '1,234.45'
+    # end
+
+
+    # it '可輸入負數' do
+    #   expect(-1234.45).to be '-1,234.45'
+    # end
+
+  end
+end
